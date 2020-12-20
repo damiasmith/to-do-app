cff79902875cab1479624740aed9e7d67fb4b41e
damia.smith <damia.v.smith@gmail.com>
2020-12-20 00:44:46 -0600

    added checklist image

--- src/containers/nav.js
+++ src/containers/nav.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Checklist from '../images/checklist.png'
 
 const Nav = () =>{
     return(
       <div className="nav">
-        <h2>Tasks To Do</h2>
-        {/* <img className="logo" src="../sample_logo.png" /> */}
+        <div className="logo">
+        <h2 className='title'>Tasks To Do</h2>
+        <img src={Checklist} height={50}  width={50}/>
+        </div>
       </div>
     )
 }
